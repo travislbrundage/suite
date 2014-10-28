be8863c7a0d32921f94d48bff93601fa683d9505
Travis Brundage <travislbrundage@gmail.com>
2014-10-28 16:46:16 -0700

    Added FootprintsControllerTest JUnit Test

---
+++ geoserver/webapp/src/test/java/com/boundlessgeo/geoserver/api/controllers/FootprintsControllerTest.java
@@ -0,0 +1,14 @@
+
+ package com.boundlessgeo.geoserver.api.controllers;  import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FootprintsControllerTest {
+
+@Test
+public void test() {
+    fail("Not yet implemented");
+}
+
+}
+
