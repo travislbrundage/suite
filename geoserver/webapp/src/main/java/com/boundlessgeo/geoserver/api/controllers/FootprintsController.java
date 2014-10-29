473ee544d688a11d876d927d8f77d27a703a3f5f
Travis Brundage <travislbrundage@gmail.com>
2014-10-29 16:26:56 -0700

    Cleaned up files

--- geoserver/webapp/src/main/java/com/boundlessgeo/geoserver/api/controllers/FootprintsController.java
+++ geoserver/webapp/src/main/java/com/boundlessgeo/geoserver/api/controllers/FootprintsController.java
@@ -3,39 +3,27 @@
  */
 package com.boundlessgeo.geoserver.api.controllers;
 
-// TODO: Clean up imports
-import com.boundlessgeo.geoserver.api.exceptions.BadRequestException;
-import com.boundlessgeo.geoserver.json.JSONArr;
-import com.boundlessgeo.geoserver.json.JSONObj;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.io.FileUtils;
-import org.geoserver.catalog.CascadeDeleteVisitor;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.LayerInfo;
-import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.catalog.Predicates;
-import org.geoserver.catalog.StoreInfo;
-import org.geoserver.catalog.WorkspaceInfo;
-import org.geoserver.catalog.util.CloseableIterator;
 import org.geoserver.config.GeoServer;
-import org.geoserver.wfs.json.GeoJSONBuilder;
-import org.geotools.geojson.GeoJSON;
+
 import org.geotools.geojson.feature.FeatureJSON;
 import org.geotools.geometry.jts.JTS;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.collection.DelegateSimpleFeatureIterator;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 
-import com.boundlessgeo.geoserver.bundle.BundleExporter;
-import com.boundlessgeo.geoserver.bundle.BundleImporter;
-import com.boundlessgeo.geoserver.bundle.ExportOpts;
-import com.boundlessgeo.geoserver.bundle.ImportOpts;
 import com.vividsolutions.jts.geom.Envelope;
-import com.vividsolutions.jts.geom.Polygon;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,36 +31,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static com.google.common.collect.Iterators.transform;
-import static org.geoserver.catalog.Predicates.equal;
-
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.filter.Filter;
-import org.opengis.geometry.Geometry;
 
 import com.google.common.base.Function;
 
-import org.geotools.data.simple.SimpleFeatureIterator;
-import org.geotools.feature.DefaultFeatureCollection;
-import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureIterator;
-import org.geotools.feature.collection.DelegateFeatureIterator;
-import org.geotools.feature.collection.DelegateSimpleFeatureIterator;
-import org.geotools.feature.collection.SimpleFeatureIteratorImpl;
-import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
-import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.geometry.Geometry;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Iterator;
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/footprints")
@@ -152,14 +121,14 @@ public class FootprintsController extends ApiController {
     
     private SimpleFeatureBuilder makeBuilder(String wsName) {
         SimpleFeatureTypeBuilder typebuilder = new SimpleFeatureTypeBuilder();
-        // TODO: Set the correct values here
+        // TODO: What values should be set here?
         typebuilder.setName(wsName);
         typebuilder.setNamespaceURI( "http://" + wsName );
         typebuilder.setSRS( "EPSG:4326" );
         typebuilder.add( "geometry", Geometry.class );
         typebuilder.add( "name", String.class );
         typebuilder.add( "title", String.class );
-        typebuilder.add( "bbox", String.class );
+        // TODO: Add value for bbox?
         
         SimpleFeatureType type = typebuilder.buildFeatureType();
         return new SimpleFeatureBuilder(type);
--- geoserver/webapp/src/test/java/com/boundlessgeo/geoserver/api/controllers/FootprintsControllerTest.java
+++ geoserver/webapp/src/test/java/com/boundlessgeo/geoserver/api/controllers/FootprintsControllerTest.java
@@ -1,26 +1,23 @@
 
  package com.boundlessgeo.geoserver.api.controllers;  import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.geoserver.config.GeoServer;
-import org.geotools.geojson.feature.FeatureJSON;
+
 import org.json.simple.JSONArray;
+
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.boundlessgeo.geoserver.api.converters.JSONMessageConverter;
